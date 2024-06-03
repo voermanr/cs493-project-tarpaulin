54b14c42999ed6e5481cb4aaca523ee0232dc0c6
Ross Voerman <voermanr@oregonstate.edu>
2024-06-03 16:23:21 -0700

    Show me a running server!

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const initDB = require('./config/initDB');
 
 const api = require('./api')
+const {connectDB} = require("./lib/mongoConnection");
 
 const app = express();
 
@@ -10,3 +12,14 @@ const port = process.env.API_PORT;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
+
+async function startYourEngines() {
+    await initDB();
+    await connectDB();
+}
+
+startYourEngines().then(() =>
+    app.listen(port, () => {
+        console.log(`Server started on port: ${port}`);
+}))
+
