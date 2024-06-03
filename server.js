46fb471f3ac184662e05408bd67263457d4c85f2
Ross Voerman <voermanr@oregonstate.edu>
2024-06-03 12:54:46 -0700

    not hello worlding yet

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 
 const api = require('./api')
+
+const app = express();
+
+const port = process.env.API_PORT;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static('public'));
